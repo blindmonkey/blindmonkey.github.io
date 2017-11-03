0fec7594cf43ee5ae10738bd07ee410b927f5905
Sergey Grabkovsky <sergey@hopper.com>
2017-11-03 16:27:54 -0400

    Remove logs and set the background to black

--- matterport/matterport.js
+++ matterport/matterport.js
@@ -1,23 +1,17 @@
 (function() {
 
     var initialize = function(matterportId, iframe) {
-        console.log('Initializing... %s', matterportId, iframe);
         if (!matterportId || !iframe) {
-            console.log('Failed');
             return;
         }
 
-        console.log('step 1');
         var showcaseLoaded = function(showcase) {
-            console.log('showcase loaded!');
             showcase.on(showcase.Events.MODEL_LOADED, function() {});
             showcase.on(showcase.Events.ENTER_PANO, function() {});
             showcase.on(showcase.Events.MOVE, function() {});
         };
 
-        console.log('step 2');
         var matterportLoaded = function() {
-            console.log('Iframe loaded');
             try {
                 // ** Replace demo applicationKey with your application key **
                 window.SHOWCASE_EMBED_SDK.connect({
@@ -32,22 +26,16 @@
             }
         };
 
-        console.log('Setting URL');
         iframe.addEventListener('load', matterportLoaded, true);
         iframe.setAttribute('src', 'https://my.matterport.com/show/?m=' + matterportId + '&hhl=0&play=1&tiles=1');
-        console.log('Now we wait');
     };
 
     window.initializeMatterport = function(matterportId) {
-        console.log('initializeMatterport(', matterportId, ')');
         var iframe = document.getElementById('matterport-content');
         if (iframe) {
-            console.log('Loaded!');
             initialize(matterportId, iframe);
         } else {
-            console.log('Not yet ready...');
             window.onload = function() {
-                console.log('onload');
                 var iframe = document.getElementById('matterport-content');
                 initialize(matterportId, iframe);
             };
