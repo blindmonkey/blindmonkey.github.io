a503c9f4a295e1b510db47ac7b5ac4acef313f6a
Sergey Grabkovsky <sergey@hopper.com>
2017-11-06 11:44:32 -0500

    Use URL params to specify the matterport ID and automatically move to panorama

--- matterport/matterport.js
+++ matterport/matterport.js
@@ -1,4 +1,15 @@
 (function() {
+    var getQueryParams = function(location) {
+        var rawParams = location.split('?').slice(1).join('?').split('&');
+        var params = {};
+        for (var i = 0; i < rawParams.length; i++) {
+            var split = rawParams[i].split('=');
+            var key = split[0];
+            var value = split.slice(1).join('=');
+            params[key] = value;
+        }
+        return params;
+    };
 
     var initialize = function(matterportId, iframe) {
         if (!matterportId || !iframe) {
@@ -9,16 +20,16 @@
             showcase.on(showcase.Events.MODEL_LOADED, function() {});
             showcase.on(showcase.Events.ENTER_PANO, function() {});
             showcase.on(showcase.Events.MOVE, function() {});
+            showcase.moveToMode({ mode: showcase.Mode.PANORAMA });
         };
 
         var matterportLoaded = function() {
             try {
                 // ** Replace demo applicationKey with your application key **
                 window.SHOWCASE_EMBED_SDK.connect({
-
-                                                  applicationKey: 'a8a9d13f-7212-4cf6-865a-f11c42a843a1',
-                                                  iframe: iframe
-                                                  })
+                    applicationKey: 'a8a9d13f-7212-4cf6-865a-f11c42a843a1',
+                    iframe: iframe
+                })
                 .then(showcaseLoaded)
                 .catch(console.error);
             } catch (e) {
@@ -30,16 +41,14 @@
         iframe.setAttribute('src', 'https://my.matterport.com/show/?m=' + matterportId + '&hhl=0&play=1&tiles=1');
     };
 
-    window.initializeMatterport = function(matterportId) {
-        var iframe = document.getElementById('matterport-content');
-        if (iframe) {
+    var matterportId = getQueryParams(location.href)['id'];
+    if (matterportId) {
+        window.onload = function() {
+            var iframe = document.getElementById('matterport-content');
             initialize(matterportId, iframe);
-        } else {
-            window.onload = function() {
-                var iframe = document.getElementById('matterport-content');
-                initialize(matterportId, iframe);
-            };
-        }
-    };
+        };
+    } else {
+        console.error('No matterport ID specified');
+    }
 
 })();
